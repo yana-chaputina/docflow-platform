85875e1e659ede7cbc11859f77c0c19287e19ad6
Iana Priazhkina <yana.chaputina@gmail.com>
2026-08-18 15:26:54 +0200

    Initial commit

---
+++ order-service/src/test/java/com/ipr/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipr.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
